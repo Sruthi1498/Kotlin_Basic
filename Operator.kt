775ce511b1313c62b52d4f09915efe61958d205c
Sruthi1498 <55286580+Sruthi1498@users.noreply.github.com>
2021-04-06 10:36:25 +0530

    Update Operator.kt

--- Operator.kt
+++ Operator.kt
@@ -1,14 +1,10 @@
-fun main() {
-    operator fun Int.times(str: String) = str.repeat(this)
-    println(2 * "Bye ")                                          // 2
-
-    operator fun String.get(range: IntRange) = substring(range)  // 3
-    val str = "Always forgive your enemies; nothing annoys them so much."
-    println(str[0..14]) // 4
-printk("adv","ärbg","aerg","khjn")
-}
-fun printk( vararg msg: String)
+fun main()
 {
-    for(i in msg)
-        println(i)
+    val i:Int = 10
+    val k:Int = 4
+    
+   
+    println(i.plus(k))
+    println(i.minus(k))
+    println(i.div(k))
 }
